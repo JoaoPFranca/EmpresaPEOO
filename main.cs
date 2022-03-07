2913b3bfefcb4f653198f9415018775e7cabc752
Hélio de Oliveira Silva Filho <79101524+helio-filho99@users.noreply.github.com>
2022-03-06 23:42:56 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,17 +6,20 @@ class Empresa {
     int opcao = 0;
     
     do {
-      opcao = Menu();
-      switch(opcao) {
-        case 1 : CriarSetor(); break;
-        case 2 : ListarSetor(); break;
-        case 3 : AtualizarSetor(); break;
+      try{
+        opcao = Menu();
+        switch(opcao) {
+          case 1 : CriarSetor(); break;
+          case 2 : ListarSetor(); break;
+          case 3 : AtualizarSetor(); break;
+          case 4 : RemoverSetor(); break;
+        }
       }
-      
-    }
-
-    while (opcao != 0);
-
+      catch (Exception erro) {
+        opcao = -1;
+        Console.WriteLine("Erro: " + erro.Message);
+      }
+    } while (opcao != 0);
   }
 
   public static int Menu() {
@@ -24,6 +27,7 @@ class Empresa {
     Console.WriteLine("01 - Criar um novo Setor");
     Console.WriteLine("02 - Listar os Setores existentes");
     Console.WriteLine("03 - Atualizar um setor");
+    Console.WriteLine("04 - Remover um setor");
     Console.WriteLine("00 - Fechar o sistema");
     Console.WriteLine("---------------------------------");
     Console.Write("Opção:  ");
@@ -35,7 +39,7 @@ class Empresa {
   
   public static void CriarSetor() {
     Console.WriteLine("----- Criar um novo setor -----");
-    Console.Write("Informe o código do setor a ser atualizado: ");
+    Console.Write("Informe o código do novo setor: ");
     int codigodosetor = int.Parse(Console.ReadLine());
     Console.Write("Informe o nome: ");
     string nome = Console.ReadLine();
@@ -55,10 +59,20 @@ class Empresa {
     Console.WriteLine("----- Atualizar um setor -----");
     Console.Write("Informe o código do setor a ser atualizado: ");
     int codigodosetor = int.Parse(Console.ReadLine());
-    Console.Write("Informe o nome do setor a ser atualizado: ");
+    Console.Write("Informe o novo nome do setor: ");
     string nome = Console.ReadLine();
     Setor obj =  new Setor(codigodosetor, nome);
     Sistema.AtualizarSetor(obj);
+    Console.WriteLine("----- Operação realizada com sucesso -----");
+  }
+
+  public static void RemoverSetor() {
+    Console.WriteLine("----- Remover um setor -----");
+    Console.Write("Informe o código do setor a ser removido: ");
+    int codigodosetor = int.Parse(Console.ReadLine());
+    string nome = "";
+    Setor obj =  new Setor(codigodosetor, nome);
+    Sistema.RemoverSetor(obj);
     Console.WriteLine("----- Operação realizada com sucesso -----");
   }
  
