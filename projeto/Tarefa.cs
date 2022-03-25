f8faf0f9f05ab41f80694273176a2ef77db0fba6
Hélio de Oliveira Silva Filho <79101524+helio-filho99@users.noreply.github.com>
2022-03-25 01:36:01 -0300

    Update Tarefa.cs

--- projeto/Tarefa.cs
+++ projeto/Tarefa.cs
@@ -4,8 +4,10 @@ public class Tarefa
 {
     public int Codigo { get; set; }
     public string Descricao { get; set; }
+  
     public override string ToString()
     {
         return $"{Codigo} - {Descricao}";
     }
+  
 }
