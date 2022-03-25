cbeab3189c64be4b3e8a91f4466a637cc009dcee
Hélio de Oliveira Silva Filho <79101524+helio-filho99@users.noreply.github.com>
2022-03-25 01:34:48 -0300

    Update Consumidor.cs

--- projeto/Consumidor.cs
+++ projeto/Consumidor.cs
@@ -1,13 +1,22 @@
 using System;
 
-public class Consumidor
+public class Consumidor : IComparable <Consumidor>
 {
-
     public int Codigo { get; set; }
     public string Nome { get; set; }
     public int Telefone { get; set; }
+
+    public string GetNome() {
+      return Nome;
+    }
+
+    public int CompareTo (Consumidor cons) {
+      return this.Nome.CompareTo(cons.GetNome());
+    }
+  
     public override string ToString()
     {
-        return $"Código do consumidor: {Codigo} - Nome do consumidor: {Nome} - Telefone do consumidor: {Telefone}";
+        return $"{Codigo} - {Nome} - {Telefone}";
     }
+  
 }
